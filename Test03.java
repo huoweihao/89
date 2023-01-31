4de5809cfec75ac639933a0aa933b6bd95c9780f
huoweihao <73099993+huoweihao@users.noreply.github.com>
2023-01-31 23:54:41 +0800

    add  two files
    hello world

---
+++ Test03.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.javase.chapter17;
+
+import java.util.Scanner;
+
+public class Test03{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		System.out.println("您输入的字符串是：" + str);
+	}
+}
